bbf1a013d96d6dbc1ab2b42a20d511f06add4848
Wanting Zhang <christinezhang@Wantings-MBP.lan>
2022-12-28 13:40:55 -0500

    190

--- reverse-bits.py
+++ reverse-bits.py
@@ -2,4 +2,9 @@
 Reverse bits of a given 32 bits unsigned integer.
 """
 
-# def reverseBits(int):
+def reverseBits(n):
+    res = 0
+    for i in range(32):
+        bit = (n >> i) & 1
+        res = res | (bit << (31 - i))
+    return res
