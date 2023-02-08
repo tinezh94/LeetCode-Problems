309df6c7b19062437dbd3eaaed1cbda80dda9725
Wanting Zhang <christinezhang@Wantings-MBP.lan>
2023-02-07 19:40:40 -0500

    runnint-sum-1d-array

--- running-sum-of-1d-array.py
+++ running-sum-of-1d-array.py
@@ -3,3 +3,15 @@ Given an array nums. We define a running sum of an array as runningSum[i] = sum(
 
 Return the running sum of nums.
 """
+
+def runningSum(nums):
+    sum = 0
+    res = []
+    for i in range(len(nums)):
+        sum += nums[i]
+        res.append(sum)
+    return res
+
+
+print(runningSum([1,2,3,4]))
+print(runningSum([1,1,1,1,1]))
